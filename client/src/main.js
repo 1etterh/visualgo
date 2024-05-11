49a7abf6e06034d40738504d00f134ba3532959c
1etterh <1etterh.dev@gmail.com>
2024-05-11 21:23:03 +0900

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import LoadScript from 'vue-plugin-load-script'
 import "vue-picocss/css/pico.min.css"
-// import axios from 'axios'
 import router from './routes/index.js'
 const app=createApp(App);
-// app.use(LoadScript);
-// app.use(axios);
 app.use(router);
 app.mount('#app');
