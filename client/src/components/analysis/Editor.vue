6667d83375accce60857383761c8b7b19e092ce8
1etterh <1etterh.dev@gmail.com>
2024-05-11 20:56:26 +0900

    refactor: move python modules into Editor.vue

--- client/src/components/analysis/Editor.vue
+++ client/src/components/analysis/Editor.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
     <Codemirror
-      v-model:value="code"
-      :options="cmOptions"
-      border
-      placeholder="Enter your code here..."
-      :height="200"
-      @change="change"
-      id="editor"
-    />
-    <Analyzer :code="code" />
-    <input type="file" @change="handleFiles" multiple>
-    <ul>  
-      <li v-for="file in files" :key="file.name">{{ file.name }}</li>
-    </ul>
-    <button @click="saveResults">Save Results</button>
+    v-model:value="code"
+    :options="cmOptions"
+    border
+    placeholder="Enter your code here..."
+    :height="200"
+    @change="change"
+    id="editor"
+  />
   </div>
+  <Button @click="runCode()" v-bind:disabled="pyodide==null">Run</Button>
+  <p v-if="pyodide==null">Loading Pyodide. Please wait...</p>
+
+  <input type="file" @change="handleFiles" multiple>
+  <ul>  
+  <li v-for="file in files" :key="file.name">{{ file.name }}</li>
+  </ul>
+
+
+  <Button @click="saveResults()"  v-bind:disabled="pyodide==null">Save Results</Button>
+
 </template>
 
 <script>
@@ -24,59 +29,119 @@ import "codemirror/mode/python/python.js";
 import "codemirror/theme/material-ocean.css";
 import "codemirror/addon/display/placeholder.js";
 import axios from 'axios';
-import Analyzer from './Analyzer.vue';
 
-import { ref } from 'vue';
+
+
+import {ref} from 'vue';
 export default {
-  name: 'Editor',
-  components: {
-    Codemirror,
-    Analyzer
-  },
-  setup() {
-    const code = ref(`3**3`);
-    return {
-      code,
-      cmOptions: {
-        styleActiveLine: true,
-        lineNumbers: true,
-        mode: "text/x-python",
-        theme: "material-ocean",
-      }
-    };
-  },
-  data() {
-    return {
-      files: []
-    };
-  },
-  methods: {
-    handleFiles(e) {
-      this.files = e.target.files;
-      console.log(this.files);
-    },
-    saveResults() {
-      let formData = new FormData();
-      formData.append('code', this.code);
-      for (let i = 0; i < this.files.length; i++) {
-        formData.append('files', this.files[i]);
-      }
-      axios.post('/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-    }
+name: 'Editor',
+components: {
+Codemirror,
+
+},
+data(){
+  return{
+    pyodide:null,
+    files:[]
   }
 }
+,
+setup(){
+  const code = ref(`3**3`);
+  return{
+    code,
+    cmOptions:
+    {
+      styleActiveLine: true,
+      lineNumbers: true,
+      mode:"text/x-python",
+      theme:"material-ocean",
+      }
+          }
+          
+        }
+      ,
+  
+mounted(){
+  this.$loadScript('https://cdn.jsdelivr.net/pyodide/v0.25.1/full/pyodide.js')
+  .then(() => {
+    console.log('Script loaded');
+    this.initPyodide();
+  })
+},
+
+methods:{
+  async initPyodide(){
+    this.pyodide = await window.loadPyodide();
+    console.log(this.pyodide.runPython(this.code));
+  },
+  runCode(){
+    let res = this.pyodide.runPython(this.code);
+    console.log(res);
+    // this.pyodide.setStdin();
+    if(this.files.length>0){
+      for (let i = 0; i < this.files.length; i++) {
+        const file = this.files[i];
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const contents = e.target.result;
+          this.pyodide.setStdin(new StdinHandler(contents.split()));
+          this.pyodide.runPython(
+            `
+            from sys import stdin as s
+            print(s.readlines())
+            `
+          );
+        };
+        reader.readAsText(file);
+      }
+      
+
+    }
+      },
+  handleFiles(e){
+    this.files = e.target.files;
+    console.log(this.files);
+},
+saveResults(){
+  let formData = new FormData();
+  formData.append('code',this.code);
+ 
+  for (let i = 0;i<this.files.length;i++){
+    formData.append('files',this.files[i]);
+  }
+  for (let pair of formData.entries()) {
+        console.log(pair[0]+ ', ' + pair[1]); 
+    }
+    axios.post('/upload', formData,{
+      headers:{
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    .then(res=>{
+      console.log(res.data)
+    })
+    .catch(err=>{
+      console.log(err)    
+  })}
+}
+
+}
+
+
+class StdinHandler{
+  constructor(results,options){
+    this.results = results;
+    this.idx = 0;
+    Object.assign(this,options);
+  }
+  stdin(){
+    return this.results[this.idx++];
+  }
+}
+
 </script>
 
 <style scoped>
-/* Your styles here */
+
 </style>
