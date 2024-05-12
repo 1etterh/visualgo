f4182348f99890a4e7a90895324ac3885c81df2d
1etterh <1etterh.dev@gmail.com>
2024-05-12 13:15:08 +0900

    fix: main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "vue-picocss/css/pico.min.css"
-import router from './routes/index.js'
+import router from './routes/index'
 const app=createApp(App);
 app.use(router);
 app.mount('#app');
